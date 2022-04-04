6a1969cfe0c597c6d6987ea1c3f4157aa25c5e12
xukeguo <82247014+xukeguo@users.noreply.github.com>
2022-04-05 01:08:59 +0800

    4

--- 2.py
+++ 2.py
@@ -1,3 +1,5 @@
+from unicodedata import decimal
+
 a = open("/Users/xkg/Documents/test.txt", "a+")
 print("http:\\\\www.baidu.com", file=a)
 a.close()
@@ -10,7 +12,7 @@ print("http:\\\\www.baidu.com")
 print(r"55555\n44444")
 print(chr(0b10000001100111111))
 print(ord('徐'))
-#导入
+# 导入
 import keyword
 
 print(keyword.kwlist)
@@ -20,17 +22,17 @@ print('标识', id(name))
 print('类型', type(name))
 print('值', name)
 print('值', name)
-n=10
-print('类型',type(n))
+n = 10
+print('类型', type(n))
 
-print('八进制',0o755)
-print('二进制',0b10101111)
-print('十六进制',0x789ad)
-a=1.1
-a2=2.1
-a3=2.2
-print(a+a2)
-print(a+a3)
-import decimal
-print(decimal('1.1')+decimal('2.2'))
+print('八进制', 0o755)
+print('二进制', 0b10101111)
+print('十六进制', 0x789ad)
+a = 1.1
+a2 = 2.1
+a3 = 2.2
+print(a + a2)
+print(a + a3)
+from decimal import Decimal
 
+print(Decimal('1.1') + Decimal('2.2'))
