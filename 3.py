3e86206209ccee6e59edd7f6163d83bf25f999bd
xukeguo <82247014+xukeguo@users.noreply.github.com>
2022-04-05 21:52:57 +0800

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,6 +1,10 @@
 # 求随机数
 from cgi import print_arguments
 from importlib.abc import Traversable
+from operator import truediv
+from pickle import TRUE
+from tkinter import FALSE
+from xmlrpc.client import Fault
 
 # level order Traver
 # level order Traversable
@@ -41,6 +45,21 @@ a.close()
 a=open("/Users/xkg/Documents/test.txt", "a+")
 print("http:\\\\www.baidu.com", file=a)
 a.close()
+a=True
+b=Fault
+print(a and b)
+print(a ,type(a))
+print(not a)
+print(not b)
+print(a+1)
+print(a-1)
+print(r"55555\n44444")
+print(chr(0b10000001100111111)) 
+print(ord('徐'))
+
+
+
+
 
 
 
