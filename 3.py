8e16b788ba99c325f1a2eb91c2d4d2070b2d50dc
xukeguo <82247014+xukeguo@users.noreply.github.com>
2022-04-05 20:43:37 +0800

    G

--- 3.py
+++ 3.py
@@ -1,16 +1,11 @@
-#求随机数
+# 求随机数
 from cgi import print_arguments
 from importlib.abc import Traversable
 
-from pip import mainprint
-
-#level order Traver
-#level order Traversable
-#level order Traversable
-#求随机数
-
-
-
+# level order Traver
+# level order Traversable
+# level order Traversable
+# 求随机数
 
 
 print_arguments
@@ -21,16 +16,13 @@ print
 from cgi import print_arguments
 from importlib.abc import Traversable
 
-#求循环数
+# 求循环数
 print_arguments
 print(8)
 pass
 import keyword
+
 print(keyword.kwlist)
 
-
-
-
-
-
-
+from decimal import Decimal
+print(Decimal('1.1') + Decimal('2.2'))
