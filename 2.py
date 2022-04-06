893c2d4ba25c9113d8043052ee9593f6e3e1944d
xukeguo <108842875@qq.com>
2022-04-06 11:39:36 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,7 +1,9 @@
 
-a = open("/Users/xkg/Documents/test.txt", "a+")
+
+a = open("/Users/xuekguo/Documents/test.txt", "a+")
 print("http:\\\\www.baidu.com", file=a)
 a.close()
+# 转义字符
 print("55555\n44444")
 print("55555\b444")
 print("55555\r44444")
@@ -73,3 +75,69 @@ print(str(a), type(str(a)))
 print(int(b), type(int(b)))
 # 布尔值
 print(int(c), type(int(c)))
+#转义字符
+#换行
+print("55555\n44444")
+#删除
+print("55555\b444")
+#回车
+print("55555\r44444")
+#制表符
+print('55555\t44444')
+#转义字符
+print("hello\"")
+#转义字符
+print("http:\\\\www.baidu.com")
+#转义字符
+print(r"55555\n44444")
+#转义字符
+print(chr(0b10000001100111111))
+#转义字符
+print(ord('徐'))
+#转义字符
+print(chr(0x789ad))
+#导入
+#import keyword
+#print(keyword.kwlist)
+#name = "科国"
+#print(name)
+#print('标识', id(name))
+#print('类型', type(name))
+#print('值', name)
+#print('值', name)
+#n = 10
+#print('类型', type(n))
+#print('八进制', 0o755)
+#print('二进制', 0b10101111)
+#print('十六进制', 0x789ad)
+#a = 1.1
+#a2 = 2.1
+#a3 = 2.2
+#open("/Users/xuekguo/Documents/test.txt", "a+")
+#只读打开文件
+#a = open("/Users/xuekguo/Documents/test.txt", "r")
+#读取文件
+#print(a.read())
+#关闭文件
+#a.close()
+#写入文件
+#a = open("/Users/xuekguo/Documents/test.txt", "a+")
+#打开百度网页
+
+d=open("http:\\\\www.baidu.com", "a+")
+print("http:\\\\www.baidu.com", file=d)
+d.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
