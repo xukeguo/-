bcea19cde895b5e5c2af765176cdcf3e7c340e95
xukeguo <108842875@qq.com>
2022-04-07 20:29:01 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -211,9 +211,12 @@ print(1<=2)        #True
 #与       
 print(True and True)  #True
 print(True and False) #False
+print(False and False) #False                      
+                         
 #或
 print(True or True)   #True
 print(True or False)  #True
+print(False or False) #False
 #非    
 print(not True)       #False
 print(not False)      #True
